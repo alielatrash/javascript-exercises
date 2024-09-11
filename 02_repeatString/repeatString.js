547aa4c21442bfc592e73f1fc95d00f83382a217
alielatrash <elatrash.ali@gmail.com>
2024-09-12 02:44:13 +0900

    add test 2 results

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,20 @@
-const repeatString = function() {
+const number = Math.floor(Math.random() * 1000);
 
-};
+const repeatString = function (word, num) {
+    if (num < 0) {
+        return "ERROR";
+    } else {
+    
+    let string = "";
+    for (i=0; i < num; i++) {
+        string += word;
+    }
+        return string;   
+    }
+
+}
+    
+repeatString('',number);
 
 // Do not edit below this line
 module.exports = repeatString;
